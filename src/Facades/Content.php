c864f213b276d74326a8cc61c5c1a42098c3d482
Niklas Rosenqvist <niklas.s.rosenqvist@gmail.com>
2020-06-09 19:06:04 +0200

    Removed code

--- src/Facades/Content.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Papyrus\Facades;
-
-class Content extends \Soma\Facade
-{
-    protected static function getFacadeAccessor()
-    {
-        return 'content';
-    }
-}
--- src/Facades/Themes.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Papyrus\Facades;
-
-class Themes extends \Soma\Facade
-{
-    protected static function getFacadeAccessor()
-    {
-        return 'themes';
-    }
-}
